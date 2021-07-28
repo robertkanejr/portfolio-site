40047970adc29cbd948bf57ed3822801badb5999
Bob <bkane90@gmail.com>
2021-07-27 21:05:52 -0400

    form edits

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 import Hero from '../components/Hero';
 import Content from '../components/Content';
 
@@ -43,31 +45,35 @@ class ContactPage extends React.Component {
             <div>
                 <Hero title={this.props.title} />
                 <Content>
-                    <Form className="py-5 my-5 px-4 mx-4" action="contact" method="post">
-                        <Form.Group>
-                            <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-                        </Form.Group>
+                    <Form className="py-5 my-5 px-3 mx-3" action="contact" method="post">
+                        <Container className="">
+                            <Form.Group>
+                                <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                                <Form.Control id="full-name" name="name" type="text" placeholder="John Doe" size="lg" value={this.state.name} onChange={this.handleChange} />
+                            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label htmlFor="email">Email</Form.Label>
-                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-                        </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label htmlFor="message">Message</Form.Label>
-                            <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange} />
-                        </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="email">Email</Form.Label>
+                                <Form.Control id="email" name="email" type="email" placeholder="email@example.com" size="lg" value={this.state.email} onChange={this.handleChange} />
+                            </Form.Group>
 
-                        <Button id="form-button" type="submit" onClick={this.handleSubmit}>
-                            Send
-                        </Button>
 
+                            <Form.Group>
+                                <Form.Label htmlFor="message">Message</Form.Label>
+                                <Form.Control id="message" name="message" as="textarea" style={{ height: '150px' }} placeholder="Write your message here." size="lg" value={this.state.message} onChange={this.handleChange} />
+                            </Form.Group>
+                        </Container>
+                        <Col className="d-flex flex-column align-items-center pt-3">
+                            <Button className="" id="form-button" type="submit" onClick={this.handleSubmit}>
+                                Send
+                            </Button>
+                        </Col>
                         {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
                         {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
                     </Form>
                 </Content>
-            </div>
+            </div >
         );
     }
 }
