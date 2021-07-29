eef1409e198ad42d26eed135edf84d227547c99e
Bob <bkane90@gmail.com>
2021-07-29 17:00:03 -0400

    more form edits

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -9,7 +9,6 @@ import Content from '../components/Content';
 
 
 class ContactPage extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -49,28 +48,35 @@ class ContactPage extends React.Component {
                         <Container>
                             <Form.Group>
                                 <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                                <Form.Control id="full-name" name="name" type="text" placeholder="John Doe" size="lg" value={this.state.name} onChange={this.handleChange} />
+                                <Form.Control id="full-name" name="name" type="text"
+                                    placeholder="John Doe" size="lg" value={this.state.name}
+                                    onChange={this.handleChange} />
                             </Form.Group>
 
                             <Form.Group>
                                 <Form.Label htmlFor="email">Email</Form.Label>
-                                <Form.Control id="email" name="email" type="email" placeholder="email@example.com" size="lg" value={this.state.email} onChange={this.handleChange} />
+                                <Form.Control id="email" name="email" type="email"
+                                    placeholder="email@example.com" size="lg" value={this.state.email}
+                                    onChange={this.handleChange} />
                             </Form.Group>
 
                             <Form.Group>
                                 <Form.Label htmlFor="message">Message</Form.Label>
-                                <Form.Control id="message" name="message" as="textarea" style={{ height: '150px' }} placeholder="Write your message here." size="lg" value={this.state.message} onChange={this.handleChange} />
+                                <Form.Control id="message" name="message" as="textarea"
+                                    style={{ height: '150px' }} placeholder="Write your message here."
+                                    size="lg" value={this.state.message} onChange={this.handleChange} />
                             </Form.Group>
                         </Container>
 
                         <Col className="d-flex flex-column align-items-center pt-3">
-                            <Button className="" id="form-button" type="submit" onClick={this.handleSubmit}>
+                            <Button className="" id="form-button" type="submit"
+                                onClick={this.handleSubmit}>
                                 Send
                             </Button>
                         </Col>
 
-                        {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
-                        {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
+                        {/* {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                        {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>} */}
                     </Form>
                 </Content>
             </div>
