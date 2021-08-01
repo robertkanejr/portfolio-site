c1a9a312a886208663a64a76c9d2556ade13c1a6
Bob <bkane90@gmail.com>
2021-08-01 15:32:45 -0400

    rebuild cards component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,54 @@
+import NRLogo from '../assets/images/NRLogo.png';
+import BILogo from '../assets/images/BILogo.png';
+import DHLogo from '../assets/images/DHLogo.jpg';
+import CryptopediaLogo from '../assets/images/CryptopediaLogo.png';
+import Tesla from '../assets/images/Tesla.png';
+import BezosBot from '../assets/images/BezosBot.png';
+
+const Cards = [
+    {
+        id: 0,
+        title: 'Biz Incubate',
+        subTitle: 'Business Incubator Bridging the Generational Gap',
+        imgSrc: BILogo,
+        link: 'https://bizincubate.com/',
+
+    },
+    {
+        id: 1,
+        title: 'New Remedies',
+        subTitle: 'First & Only Federally Compliant CBD Franchise',
+        imgSrc: NRLogo,
+        link: 'https://newremediescbd.com/',
+    },
+    {
+        id: 2,
+        title: 'Dailee Holdings LLC.',
+        subTitle: 'Compliant Solutions for the Cannabinoid Industry',
+        imgSrc: DHLogo,
+        link: 'https://daileecbd.com/',
+    },
+    {
+        id: 3,
+        title: 'Cryptopedia',
+        subTitle: 'Cryptocurrency Trading Simulator',
+        imgSrc: CryptopediaLogo,
+        link: 'https://cryptopedia.netlify.app/',
+    },
+    {
+        id: 4,
+        title: 'Nikola Tesla Electrical Storm Tracker',
+        subTitle: 'Track Storms Using Google Maps API',
+        imgSrc: Tesla,
+        link: 'https://Nikola-Tesla-electric.netlify.app/',
+    },
+    {
+        id: 5,
+        title: 'Bezos World Domination Game',
+        subTitle: 'Futuristic Shooter Game',
+        imgSrc: BezosBot,
+        link: 'https://Bezos-domination-game.netlify.app/',
+    },
+]
+
+export default Cards;
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,61 +1,9 @@
 import React from 'react';
-// import Card from './Card';
+import Cards from './Cards';
 import CardInfo from './CardInfo';
-import NRLogo from '../assets/images/NRLogo.png';
-import BILogo from '../assets/images/BILogo.png';
-import DHLogo from '../assets/images/DHLogo.jpg';
-import CryptopediaLogo from '../assets/images/CryptopediaLogo.png';
-import Tesla from '../assets/images/Tesla.png';
-import BezosBot from '../assets/images/BezosBot.png';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
-
-const items = [
-    {
-        id: 0,
-        title: 'Biz Incubate',
-        subTitle: 'Business Incubator Bridging the Generational Gap',
-        imgSrc: BILogo,
-        link: 'https://bizincubate.com/',
-
-    },
-    {
-        id: 1,
-        title: 'New Remedies',
-        subTitle: 'First & Only Federally Compliant CBD Franchise',
-        imgSrc: NRLogo,
-        link: 'https://newremediescbd.com/',
-    },
-    {
-        id: 2,
-        title: 'Dailee Holdings LLC.',
-        subTitle: 'Compliant Solutions for the Cannabinoid Industry',
-        imgSrc: DHLogo,
-        link: 'https://daileecbd.com/',
-    },
-    {
-        id: 3,
-        title: 'Cryptopedia',
-        subTitle: 'Cryptocurrency Trading Simulator',
-        imgSrc: CryptopediaLogo,
-        link: 'https://cryptopedia.netlify.app/',
-    },
-    {
-        id: 4,
-        title: 'Nikola Tesla Electrical Storm Tracker',
-        subTitle: 'Track Storms Using Google Maps API',
-        imgSrc: Tesla,
-        link: 'https://Nikola-Tesla-electric.netlify.app/',
-    },
-    {
-        id: 5,
-        title: 'Bezos World Domination Game',
-        subTitle: 'Futuristic Shooter Game',
-        imgSrc: BezosBot,
-        link: 'https://Bezos-domination-game.netlify.app/',
-    },
-]
 
 class Carousel extends React.Component {
     render() {
@@ -99,13 +47,15 @@ class Carousel extends React.Component {
             ]
 
         }
+
         return (
             <Slider {...settings}>
-                {items.map((item) => {
+                {Cards.map((item) => {
                     return (
                         <div className="cards">
                             <img className="b-card-image" src={item.imgSrc} alt={item.imgSrc} />
-                            <div>{<CardInfo title={item.title} subTitle={item.subTitle} link={item.link} />}</div>
+                            <div>{<CardInfo title={item.title} subTitle={item.subTitle} link={item.link} />}
+                            </div>
                         </div>
                     )
                 })}
