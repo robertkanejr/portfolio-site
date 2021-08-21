bc8b652e54e93e11ccaf54c4a7053748d7412cb3
Bob <bkane90@gmail.com>
2021-08-21 18:10:54 -0400

    about edits

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -13,9 +13,9 @@ function AboutPage(props) {
                 <Row className="justify-content-center p-0 m-0">
                     <img src={Headshot} className='pb-5' alt='Me' />
                     <Content>
-                        <p>Hey there! I'm Bob Kane, a fullstack web developer based out of Brooklyn, NY with a background in finance and marketing.
-                            I'm currently working as a freelance web developer while I pursue my next full-time role. As an Ironhack bootcamp graduate,
-                            I'm most familar with the technologies used in the MERN stack (MongoDB, Express, React, Node.js) as well as JavaScript, HTML, CSS, and WordPress. </p>
+                        <p>Hey there! I'm Bob Kane, a full stack software engineer based out of Brooklyn, NY with hands-on experience designing and developing innovative
+                            web applications from concept to deployment. I'm a motivated self-starter with a passion for working with clients and colleagues to build practical, creative,
+                            and user-friendly solutions. I'm also a business professional with 8 years of results-driven leadership experience prior to pivoting into web development. </p>
 
                         <p>Critical thinking and problem solving are in my nature, so it's fitting that I can fully integrate those skills into my career through coding.
                             I'm very solutions-oriented, and I instinctively try to understand why and how things work the way they do.
@@ -23,11 +23,11 @@ function AboutPage(props) {
 
                         <p> As a student of life, I'm deeply passionate about self-improvement and have an insatiable appetite for learning.
                             In my neverending quest for knowledge, I'm always seeking to better myself and continually learn new things in all aspects of life.
-                            In terms of web development, these qualities allow me to push the limitations of my current capabilities while always striving to become a better programmer.</p>
+                            In terms of engineering, these qualities allow me to push the limitations of my current capabilities while always striving to become a better programmer.</p>
 
                         <p> When I'm not busy coding, you can usually find me enjoying some of my favorite hobbies.
-                            These include reading, meditation, yoga, exercising, snowboarding, DJing, fishing, cooking, and traveling.
-                            Besides programming languages, I'm also currently learning how to speak Spanish. Eventually I strive to be bilingual and add that to my already dynamic skillset.</p>
+                            These include reading, meditation, yoga, exercising, snowboarding, DJing, fishing, cooking, and traveling. If you're interested in connecting,
+                            please feel free to drop me a line on the contact page!</p>
                     </Content>
                 </Row>
             </Container>
